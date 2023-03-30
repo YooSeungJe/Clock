8eedca495d64cfb1594d1a332940ef9eef88105c
YooSeungJe <127471220+YooSeungJe@users.noreply.github.com>
2023-03-30 22:42:05 +0900

    Add files via upload

---
+++ 아날로그 시계/tur.js
@@ -0,0 +1,24 @@
+function setClock(){
+    var dateInfo = new Date(); //
+    var hour = modifyNumber(dateInfo.getHours()-6);
+    var min = modifyNumber(dateInfo.getMinutes());
+    var sec = modifyNumber(dateInfo.getSeconds());
+    var year = dateInfo.getFullYear();
+    var month = dateInfo.getMonth()+1; //monthIndex를 반환해주기 때문에 1을 더해준다.
+    var date = dateInfo.getDate();
+    document.getElementById("time").innerHTML = hour + ":" + min  + ":" + sec;
+    document.getElementById("date").innerHTML = year + " / " + month + " / " + date;
+}
+function modifyNumber(time){
+    if(parseInt(time)<10){
+        return "0"+ time;
+    } else if(parseInt(time)>24) {
+        return time-24;
+    }
+    else
+        return time;
+}
+window.onload = function(){
+    setClock();
+    setInterval(setClock,1000); //1초마다 setClock 함수 실행
+}
